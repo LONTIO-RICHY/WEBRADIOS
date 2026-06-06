4a7aa45fcf86426b0c7451a32e0f9cd58a8417d8
LONTIO2004 <lontiokessel@gmail.com>
2026-06-06 13:13:59 +0100

    petit arrangement

--- src/components/Listederoulante.jsx
+++ src/components/Listederoulante.jsx
@@ -10,8 +10,8 @@ function Listederoulante() {
   <label htmlFor="toggle-list" className="peer-checked:hidden block cursor-pointer w-2"><span className="w-8 h-8 scale-150"><Menu/></span></label>
   <label htmlFor="toggle-list" className="hidden peer-checked:block cursor-pointer w-2"><X/></label>
   <ul className="opacity-0 scale-95 transition-all duration-1000 ease-out peer-checked:opacity-100 peer-checked:scale-100 absolute shadow-md p-2 mt-2">
-        <Link to="Studio" className="block hover:bg-gray-500 text-black p-3">Commentaire</Link>
-        <a href="#" className="block hover:bg-gray-500 text-black  p-3 ">Podcasts</a>
+        <Link to="Studio" className="block hover:bg-gray-500 text-black p-3">Podcasts(streaming)</Link>
+        <a href="#" className="block hover:bg-gray-500 text-black  p-3 ">commentaire</a>
         <a href="#" className="block hover:bg-gray-500 text-black p-3 ">Historisation</a>
         <a href="#" className="block hover:bg-gray-500 text-black p-3 ">Actualité</a>
         <a href="#" className="block hover:bg-gray-500 text-black p-3 ">Liste music</a>
